1ea3870001411fcf6591d227c4ca2558b4b67b44
anhttl20411 <anhttl20411@st.uel.edu.vn>
2022-11-25 00:50:45 +0700

    Initial commit

---
+++ app/src/main/java/com/example/sunhappy/ui/aboutsunhappy/AboutsunhappyViewModel.java
@@ -0,0 +1,7 @@
+package com.example.sunhappy.ui.aboutsunhappy;
+
+import androidx.lifecycle.ViewModel;
+
+public class AboutsunhappyViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/sunhappy/ui/category/HomeViewModel.java
@@ -0,0 +1,7 @@
+package com.example.sunhappy.ui.category;
+
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
